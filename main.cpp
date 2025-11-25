87872fb09ea4705f42b1c4973b46a5fe8e77f866
cz0729zc <3273447162@qq.com>
2025-11-25 19:04:25 +0800

    P17:指针和引用

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 #include "Log.h"
 
-int main(int, char**){
-    LogInit();
-    const char* ptr = nullptr;
-    if (ptr){  
-        Log("Pointer is valid");
-        // Log(ptr);
-    }else{
-        Log("Pointer is null");
-    }
+void FunctionA(int* value){
+    (*value)++;
+}
 
+void FunctionB(int& value){
+    value += 2;
+}
+
+int main(int, char**){
+    int a = 5;
+    int b = 10;
+
+    int& ref = a;
+    ref = b;
+    ref++;
+    std::cout << "a: " << a << ", b: " << b << std::endl;
+
+    // FunctionA(&a);
+    // std::cout << a << std::endl;
+    // FunctionB(a);
+    // std::cout << a << std::endl;
+    return 0;
 }
                                                                                                                     
