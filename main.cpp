32547f8e7feeeb4c14d2aa62a90401fd10700de6
cz0729zc <3273447162@qq.com>
2025-11-30 15:38:10 +0800

    P30:访问控制

--- main.cpp
+++ main.cpp
@@ -1,46 +1,36 @@
 #include <iostream>
 // #include "Log.h"
 
-class Printable
+class Entity 
 {
+protected:
+    int X, Y;
+    void print() {};
 public:
-    virtual std::string GetClassName() = 0;  
-};
-
-class Entity : public Printable
-{
-public:
-    virtual std::string GetName() { return "Entity"; }
-    std::string GetClassName() override { return "Entity"; } // 实现纯虚函数，否则 Entity 也是抽象类（不能 new）
+    Entity()
+    {
+        X = 0;
+        Y = 0;
+        print();
+    }
 };
 
 class Player : public Entity
 {
-private:
-    std::string m_Name;
 public:
-    Player(const std::string& name)
-        : m_Name(name)
+    Player()
     {
-
+        X = 10;
+        Y = 20;
+        print();
     }
-    // std::string GetName() override { return m_Name; }
-    std::string GetClassName() override { return "Player"; } 
 };
 
-void Print (Printable* e)
-{   
-    std::cout << e->GetClassName() << std::endl;
-}
 
 int main()
 {
-    Entity* e = new Entity();
-    Print(e);
-    Player* p = new Player("Bob");
-    Print(p);
-    // Entity* e2 = p;
-    // Print(e2);
+    Entity e;
+    e.print();
     return 0;
 }
                                                                                                                     
