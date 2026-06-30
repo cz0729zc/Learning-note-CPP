19f325272fb7236776d5a062b784affca4f703f6
cz0729zc <3273447162@qq.com>
2026-06-30 21:47:07 +0800

    P49:添加GLFW库

--- main.cpp
+++ main.cpp
@@ -1,39 +1,31 @@
 #include <iostream>
-#include <string>
-#include <vector>
-
-class Vertex
-{
-public:
-    float x, y, z;
-
-    Vertex(float x, float y, float z) 
-        : x(x), y(y), z(z) 
-    {
-
-    }
-
-    Vertex(const Vertex& vertex)
-        : x(vertex.x), y(vertex.y), z(vertex.z)
-    {
-        std::cout << "Copied!" << std::endl;
-    }
-};
-
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    std::vector<Vertex> vertices;
-    /* 优化方式一：提前预留空间 */
-    // vertices.reserve(3); 
-    
-    /* 这里出现6次复制的原因有两个：一是空vector第一次插入时会分配容量，之后容量不够还会扩容并复制已有元素；二是Vertex先创建在main函数栈上，传入vector时又会复制一次。*/
-    // vertices.push_back(Vertex(1, 2, 3));
-    // vertices.push_back(Vertex(4, 5, 6));
-    // vertices.push_back(Vertex(7, 8, 9));
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
 
-    /* 优化方式二：使用emplace_back避免不必要的复制，相当于告诉vector直接在容器内部构造对象，而不是先构造再复制 */
-    vertices.emplace_back(1, 2, 3);
-    vertices.emplace_back(4, 5, 6);
-    vertices.emplace_back(7, 8, 9);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Window", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
